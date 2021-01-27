0ad06f88bdc28a6e57b803e5d56b0cf7ade93cb0
pratamawijaya <set.mnemonix@gmail.com>
2021-01-27 10:58:20 +0700

    variable

--- hello-world/main.go
+++ hello-world/main.go
@@ -1,7 +1,14 @@
 package main
 
+// import fmt https://golang.org/pkg/fmt/
 import "fmt"
 
 func main()  {
-	fmt.Println("Hello World")
+	var firstName string = "Pratama"
+	var lastName string
+
+	lastName = "Wijaya"
+
+	fmt.Printf("Hello World %s %s", firstName, lastName)
+
 }
