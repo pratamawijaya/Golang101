098269d061b8214f5426c297225798be1afe19a5
pratamawijaya <set.mnemonix@gmail.com>
2021-01-27 11:23:37 +0700

    temporary variable pada kondisional

--- hello-world/main.go
+++ hello-world/main.go
@@ -43,4 +43,14 @@ func main()  {
 		fmt.Println("tidak lulus, nilai anda ", point)
 	}
 
+	var nilai = 8840.0
+
+	if percent := nilai / 100; percent >= 100 {
+    		fmt.Printf("%.1f%s perfect!\n", percent, "%")
+	} else if percent >= 70 {
+		fmt.Printf("%.1f%s good\n", percent, "%")
+	} else {
+    		fmt.Printf("%.1f%s not bad\n", percent, "%")
+	}
+
 }
