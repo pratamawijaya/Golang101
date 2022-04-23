7ceaaff8eb2821ffceb47eeabba570e92051c202
pratamawijaya <set.mnemonix@gmail.com>
2022-04-23 09:19:19 +0700

    category findById repository impl

--- 7_rest_api/repository/category_repository.go
+++ 7_rest_api/repository/category_repository.go
@@ -10,7 +10,7 @@ type CategoryRepository interface {
 	Save(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category
 	Update(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category
 	Delete(ctx context.Context, tx *sql.Tx, category domain.Category) 
-	FindById(ctx context.Context, tx *sql.Tx, categoryId int) domain.Category
+	FindById(ctx context.Context, tx *sql.Tx, categoryId int) (domain.Category, error)
 	FindAll(ctx context.Context, tx *sql.Tx) []domain.Category
 }
 	
--- 7_rest_api/repository/category_repository_impl.go
+++ 7_rest_api/repository/category_repository_impl.go
@@ -1,24 +1,24 @@
 package repository
 
 import (
+	"context"
+	"database/sql"
+	"errors"
 	"restapi/helper"
 	"restapi/model/domain"
-	"database/sql"
-	"context"
 )
 
 type CategoryRepositoryImpl struct {
-
 }
 
 func (repository *CategoryRepositoryImpl) Save(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category {
 	SQL := "INSERT INTO category (name) VALUES (?)"
 	result, err := tx.ExecContext(ctx, SQL, category.Name)
-	
+
 	helper.PanicIfError(err)
-	
+
 	id, err := result.LastInsertId()
-	
+
 	helper.PanicIfError(err)
 
 	category.Id = id
@@ -29,7 +29,7 @@ func (repository *CategoryRepositoryImpl) Save(ctx context.Context, tx *sql.Tx, 
 func (repository *CategoryRepositoryImpl) Update(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category {
 	SQL := "UPDATE category SET name = ? WHERE id = ?"
 	_, err := tx.ExecContext(ctx, SQL, category.Name, category.Id)
-	
+
 	helper.PanicIfError(err)
 
 	return category
@@ -38,7 +38,23 @@ func (repository *CategoryRepositoryImpl) Update(ctx context.Context, tx *sql.Tx
 func (repository *CategoryRepositoryImpl) Delete(ctx context.Context, tx *sql.Tx, category domain.Category) {
 	SQL := "DELETE FROM category WHERE id = ?"
 	_, err := tx.ExecContext(ctx, SQL, category.Id)
-	
+
 	helper.PanicIfError(err)
 
+}
+
+func (repository *CategoryRepositoryImpl) FindById(ctx context.Context, tx *sql.Tx, categoryId int) (domain.Category, error) {
+	SQL := "SELECT id, name FROM category WHERE id = ?"
+	rows, err := tx.QueryContext(ctx, SQL, categoryId)
+
+	helper.PanicIfError(err)
+
+	category := domain.Category{}
+	if rows.Next() {
+		err := rows.Scan(&category.Id, &category.Name)
+		helper.PanicIfError(err)
+		return category, nil
+	} else {
+		return category, errors.New("Category not found")
+	}
 }
